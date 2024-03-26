0a9c6c1575a090deb3852c49435d6f19c8cb7811
SamueleFerraro <ellsamueleferraro@gmail.com>
2024-03-26 18:54:38 +0100

    Update KittCarPWM.vhd
    Ho commentato qua e là per gli analfabeti come me. Ho messo un paio di domande che sono superflue per il codice ma spero utili per noi (da buon rompiscatole quale sono). Sulle cose sulla quale non sono certo ho messo "(credo)"

--- LAB1/KittCarPWM_JACK/KittCarPWM.vhd
+++ LAB1/KittCarPWM_JACK/KittCarPWM.vhd
@@ -28,7 +28,7 @@ architecture Behavioral of KittCarPWM is
   -- - routare le uscite dei PWM sulle uscite giuste
 	constant DELTA_T					: positive  := MIN_KITT_CAR_STEP_MS * 1000000 / CLK_PERIOD_NS;
 	constant BITS_OF_COUNTER	: positive  := integer(ceil(log2(real(DELTA_T))));
-  -- I nostri PWM hanno DC : Ton / (Period + 1): in sintesi, se Ton è 0,
+  -- I nostri PWM hanno DC (Duty Cycle) : Ton / (Period + 1): in sintesi, se Ton è 0,
   -- l'uscita è fissa a 0. Quindi per avere TAIL_LENGTH diversi DC bisogna
   -- avere BITS_OF_PWM tale per accomodare TAIL_LENGTH+1.
 	constant BITS_OF_PWM			: positive  := integer(ceil(log2(real(TAIL_LENGTH + 1))));
@@ -38,22 +38,33 @@ architecture Behavioral of KittCarPWM is
   -- impostare Ton al max per avere il LED acceso fisso
   constant PERIOD_INIT      : integer   := (2**BITS_OF_PWM) - 2;
   --constant STEP_OF_PWM      : unsigned := to_unsigned( (2**BITS_OF_PWM)/TAIL_LENGTH, BITS_OF_PWM); ---- piccolo controllo se e' necessario floor o qualche altra cazzata
+  
+  --SF: range <> sta a dire che il range è uncostrained, quindi non definito
+	type TonPWM is array (integer range <>) of unsigned(BITS_OF_PWM-1 downto 0);
 
-	type TonPWM is array (integer range <>) of unsigned(BITS_OF_PWM-1 downto 0); 
-	signal counter						: unsigned(BITS_OF_COUNTER downto 0)				:= (Others => '0');
-	signal counter_sw					: unsigned(sw'RANGE)												:= (Others => '0');
-	signal sw_reg    					: unsigned(sw'RANGE)												:= (Others => '0');
---	signal led_reg						: unsigned(leds'RANGE)											:= (0 => '1', Others => '0');
-	signal index_led					: integer range leds'RANGE									:= 0;
-	signal direction					: std_logic																	:= '0';
+	signal counter						: unsigned(BITS_OF_COUNTER downto 0)			:= (Others => '0');
+	signal counter_sw					: unsigned(sw'RANGE)							:= (Others => '0');
+	signal sw_reg    					: unsigned(sw'RANGE)							:= (Others => '0');
+--	signal led_reg						: unsigned(leds'RANGE)							:= (0 => '1', Others => '0');
+	signal index_led					: integer range leds'RANGE						:= 0;
+	signal direction					: std_logic										:= '0';
+
+-- SF: PWMs_input contiene le info con cui vogliamo regolare i vari PWM, ovvero i vari Ton. 
+--	   La lunghezza è fissata a TAIL_LENGTH seguendo il proposito del commento precendente (v1.5).
+--	   Non mi è chiarissimo il perchè sia (TAIL_LENGTH downto 1) invece di (TAIL_LENGTH - 1 downto 0), ma alla fine è la stessa cosa.  
 	signal PWMs_input					: TonPWM(TAIL_LENGTH downto 1)      			:= (Others => (Others => '0'));
-  type tail_type is array (leds'RANGE) of unsigned(BITS_OF_PWM-1 downto 0);
-  signal tail               : tail_type 																:= (Others => (Others => '0'));
-  signal PWMs_output        : std_logic_vector(TAIL_LENGTH-1 downto 0)	:= (Others => PWM_INIT);
-	
+
+-- SF: tail_type serve a creare un array come leds che sia in grado di contenere l'effetto "decadimento" della coda.
+--     Secondo me si poteva usare il tipo TonPWM, ma il tail_type non costa nulla e rende il codice molto più leggibile.
+	type tail_type is array (leds'RANGE) of unsigned(BITS_OF_PWM-1 downto 0);
+  	signal tail               : tail_type 												:= (Others => (Others => '0'));
+-- SF: PWMs_output sono tutte le onde quadre che escono dai PWM   	
+	signal PWMs_output        : std_logic_vector(TAIL_LENGTH-1 downto 0)				:= (Others => PWM_INIT);
+	 
+
 	component PulseWidthModulator is
   Generic(
-    BIT_LENGTH  : integer range 1 to 16 := 8; -- Length of std_logic_vector of this entity
+    BIT_LENGTH  : integer range 1 to 16 := 8;  -- Length of std_logic_vector of this entity
     T_ON_INIT   : positive  := 64;             -- Initial value of the register T_ON_eff
     PERIOD_INIT : positive  := 128;            -- Initial value of the register PERIOD_eff
     PWM_INIT    : std_logic := PWM_INIT        -- Initial value of the PWM ouput
@@ -75,10 +86,10 @@ begin
 	begin
 		if reset = '1' then
 			counter			<= (Others => '0');
-			counter_sw	<= (Others => '0');
+			counter_sw		<= (Others => '0');
 			index_led		<= 0;
 			sw_reg			<= unsigned(sw);
-			tail				<= (Others => (Others => '0'));
+			tail			<= (Others => (Others => '0'));
 		elsif rising_edge(clk) then
 			counter <= counter + 1;
 			if counter = DELTA_T then
@@ -88,14 +99,15 @@ begin
 					counter_sw 	<= (Others => '0');
 					sw_reg			<= unsigned(sw);
 					
-					if direction= '0' then
+					if direction= '0' then --scorro verso sinistra
+					--SF: Jack che tocco di stile con 'LEFT, stima completa
 						if index_led = leds'LEFT then
 							direction	<= '1';
 							index_led <= index_led - 1;
 						else
-							index_led <= index_led + 1;
+							index_led <= index_led + 1; --SF: +1 vuol dire andare a sinistra, va preso come riferimento la out port leds
 						end if;
-					else -- direction = '1'
+					else -- direction = '1', scorro verso destra
 						if index_led = leds'RIGHT then
 							direction	<= '0';
 							index_led <= index_led + 1;
@@ -103,12 +115,15 @@ begin
 							index_led <= index_led - 1;
 						end if;
 					end if;
-					
+
+					-- SF: Creazione della coda pensata come [TAIL_LENGTH, TAIL_LENGTH - 1, ...] (un po come me la avevi spiegata quando ci siamo visti)
+					--     Credo che questo sia codice comune con la versione 16 PWM. 
+					--     Domanda di approfondimento: si poteva fare usando operazioni vettore di VHDL secondo voi? Io non ho trovato una versione equivalente.
 					for I in tail'RANGE loop
 						if I = index_led then
-							tail(I) <= to_unsigned(TAIL_LENGTH, BITS_OF_PWM); -- Non mi piace, vorrei un attribute dell'elemento i-esimo di tail (ho letto tipo 'LENGTH(I) o 'LENGTH(1)
+							tail(I) <= to_unsigned(TAIL_LENGTH, BITS_OF_PWM); --JACK: Non mi piace, vorrei un attribute dell'elemento i-esimo di tail (ho letto tipo 'LENGTH(I) o 'LENGTH(1)
 						elsif tail(I) /= 0 then
-							tail(I) <= tail(I) - 1;
+							tail(I) <= tail(I) - 1; --SF: Questa riga funziona usando il signal commit, assurdo
 						end if;
 					end loop;
 
@@ -135,13 +150,16 @@ begin
 --    end loop;
 --    wait;
 --  end process inputs;
-  
+
+
+-- SF: qui viene creato l'array che contiene le configurazioni per i PWM: [HIGH,..., LOW] dove HIGH contiene il valore massimo della coda
   inputs_gen : for I in PWMs_input'RANGE generate
   	first_gen : if I = PWMs_input'HIGH generate
   		PWMs_input(I) <= to_unsigned( ((2**BITS_OF_PWM) - 1), BITS_OF_PWM );
   	end generate first_gen;
   	others_gen : if I /= PWMs_input'HIGH generate
-  		PWMs_input(I) <= to_unsigned( to_integer(PWMs_input(I+1)) - to_integer(PWMs_input(I+1) / (I+1) ), BITS_OF_PWM );
+	-- SF: qui c'è il decadimento dei Ton. Domanda: Questa divisione va bene? La fa vivado o Basys 3?
+  		PWMs_input(I) <= to_unsigned(to_integer(PWMs_input(I+1)) - to_integer(PWMs_input(I+1) / (I+1) ), BITS_OF_PWM );
   	end generate others_gen;
   end generate inputs_gen;
   
@@ -158,6 +176,7 @@ begin
     Port Map(
       reset   => reset,
       clk     => clk,
+	  --SF: qui si ha I+1 per rimanere coerenti il range (TAIL_LENGTH downto 1) di prima (credo)
       Ton     => std_logic_vector(PWMs_input(I+1)),
       Period  => std_logic_vector(to_unsigned(PERIOD_INIT, BITS_OF_PWM)),
       PWM     => PWMs_output(I)
@@ -176,8 +195,8 @@ begin
 --  end process routing;
 
   routing_gen : for I in tail'RANGE generate
-  	with tail(I) select leds(I) <= 	'0' when to_unsigned(0, BITS_OF_PWM),
-  																	PWMs_output(to_integer(tail(I) - 1)) when Others;
+  	with tail(I) select leds(I) <= 	'0' 								 when to_unsigned(0, BITS_OF_PWM),
+  									PWMs_output(to_integer(tail(I) - 1)) when Others;
 --    leds(I) <= PWMs_output(to_integer(tail(I))) when tail(I) /= 0 else
 --               '0';
   end generate;
