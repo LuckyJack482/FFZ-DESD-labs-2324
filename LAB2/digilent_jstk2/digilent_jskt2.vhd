533fc48e017d6eb96bfaf44e84d5cde58d9b8202
luckyjack482@gmail.com <luckyjack482@gmail.com>
2024-04-17 16:45:44 +0200

    Commenti del digilent_jstk2

--- LAB2/digilent_jstk2/digilent_jskt2.vhd
+++ LAB2/digilent_jstk2/digilent_jskt2.vhd
@@ -3,9 +3,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity digilent_jstk2 is
 	Generic (
-		DELAY_US		: integer := 25;    -- Delay (in us) between two packets
-		CLKFREQ		 	: integer := 100_000_000;  -- Frequency of the aclk signal (in Hz)
-		SPI_SCLKFREQ 		: integer := 5_000 -- Frequency of the SPI SCLK clock signal (in Hz)
+		DELAY_US		: integer := 25;    		-- Delay (in us) between two packets
+		CLKFREQ		 	: integer := 100_000_000;  	-- Frequency of the aclk signal (in Hz)
+		SPI_SCLKFREQ 		: integer := 5_000 		-- Frequency of the SPI SCLK clock signal (in Hz)
 	);
 	Port ( 
 		aclk 			: in	std_logic;
@@ -39,133 +39,187 @@ architecture Behavioral of digilent_jstk2 is
 	constant CMDSETLEDRGB		: std_logic_vector(7 downto 0) := x"84";
 	-- Do not forget that you MUST wait a bit between two packets. See the JSTK2 datasheet (and the SPI IP-Core README).
 	------------------------------------------------------------
+	-- Determining DELAY_CYCLES using the constraints DELAY_US, CLKFREQ and SPI_SCLKFREQ in the generic of the module
 	constant DELAY_CYCLES 		: integer := DELAY_US * (CLKFREQ / 1_000_000) + CLKFREQ / SPI_SCLKFREQ;
 
-
+	/*
+	Declaration of delay_counter, which is  used to wait for TX_DELAY clock
+	period in the WAITING m_state.
+	*/
 	signal delay_counter 		: integer range 0 to DELAY_CYCLES 	:= 0;
-	signal jstk_x_reg, jstk_y_reg	: std_logic_vector(jstk_x'RANGE) 	:= (Others => '0');
-	--signal btns_reg			: std_logic_vector(1 downto 0)		:= (Others => '0');
 
+	/*
+	Declaration of jstk_x_reg and jstk_y_reg registers to synchronize outputs
+	*/
+	signal jstk_x_reg		: std_logic_vector(jstk_x'RANGE) 	:= (Others => '0');
+	signal jstk_y_reg		: std_logic_vector(jstk_y'RANGE) 	:= (Others => '0');
+
+	/*
+	 # Slave: joystick data from joystick to datapath
+	 Definition of the slave_FSM states in the s(lave)_state_type and
+	 declaration of s(lave)_state signal. The states' name describe their
+	 purpose.
+	 There is no RESET state since it's not required: since there is no s_axis_tready,
+	 the s_FSM is always receiving data from the joystick, it only stops while aresetn='0':
+	 so the various resets are made only when aresetn='0'.
+	 L stands for low significant bits, H stands for high significant bits.
+	 FSB stands for fsButtons as in the digilent2 jstk manual.
+	*/ 
 	type s_state_type is (READING_X_L, READING_X_H, READING_Y_L, READING_Y_H, READING_FSB);
 	signal s_state	: s_state_type	:= READING_X_L;
-	type m_state_type is (RESET, WAITING, WRITE_CMD, WRITE_R, WRITE_G, WRITE_B, WRITE_DUMMY);
+
+	/*
+	 # Master: leds from datapath to joystick
+	 Definition of the master_FSM states in the m(aster)_state_type and
+	 declaration of m(aster)_state signal. The states' name describe their
+	 purpose, precisely:
+	 - WAITING is designed to comply the specification described at line 6;
+	 - the others are called WRITTEN_... since when entering the state, the
+	   output commutes to the corresponding value ( e.g. when entering 
+	   WRITTEN_R, the m_axis_tdata is commuting simultaneously to the 
+	   leds_r value )
+	 - WRITTEN_DUMMY is the state in which we send a valid 0x00 data to
+	   comply with the packet required by the joystick module as written in
+	   the manual
+	*/ 
+	type m_state_type is (RESET, WAITING, WRITTEN_CMD, WRITTEN_R, WRITTEN_G, WRITTEN_B, WRITTEN_DUMMY);
 	signal m_state	: m_state_type	:= RESET;
 
-	--constant zeros			: std_logic_vector(m_axis_tdata'HIGH - JSTK_BITS downto 0) := (Others => '0');
-
 begin
+
+	/*
+	# JSTK2 Protocol: AXI4-Stream Slave, joystick data from joystick to datapath
+	The sensitivity list presents only aclk and aresetn since the module is
+	synchronous with an asynchronous reset.
+	*/
 	s_FSM : process (aclk, aresetn) 
 	begin 
+		/* Asynchronous reset */
 		if aresetn = '0' then 
-			s_state <= READING_X_L; 
-			jstk_x		<= (Others => '0');
+			s_state <= READING_X_L; 		-- State reset
+
+			jstk_x		<= (Others => '0');	-- Outputs reset
 			jstk_y		<= (Others => '0');
 			jstk_x_reg	<= (Others => '0');
 			jstk_y_reg	<= (Others => '0');
 			btn_jstk   	<= '0';
                         btn_trigger	<= '0';
+
 		elsif rising_edge(aclk) then 
 			case s_state is 
 				when READING_X_L => 
 					if s_axis_tvalid = '1' then
-						jstk_x_reg(7 downto 0)	<= s_axis_tdata;
-						s_state	<= READING_X_H;
+						jstk_x_reg(7 downto 0)	<= s_axis_tdata;		-- Saving valid 8 LSBs in registers (x coord.)
+						s_state	<= READING_X_H;					-- Switching state only when data is valid
 					end if;
 
 				when READING_X_H => 
 					if s_axis_tvalid = '1' then
-						jstk_x_reg(9 downto 8)	<= s_axis_tdata(1 downto 0);
-						s_state	<= READING_Y_L;
+						jstk_x_reg(9 downto 8)	<= s_axis_tdata(1 downto 0);	-- Saving valid 2 MSBs in registers (x coord.)
+						s_state	<= READING_Y_L;					-- Switching state only when data is valid
 					end if;
 
 				when READING_Y_L => 
 					if s_axis_tvalid = '1' then
-						jstk_y_reg(7 downto 0)	<= s_axis_tdata;
-						s_state	<= READING_Y_H;
+						jstk_y_reg(7 downto 0)	<= s_axis_tdata;		-- Saving valid 8 LSBs in registers (y coord.)
+						s_state	<= READING_Y_H;					-- Switching state only when data is valid
 					end if;
 
 				when READING_Y_H => 
 					if s_axis_tvalid = '1' then
-						jstk_y_reg(9 downto 8)	<= s_axis_tdata(1 downto 0);
-						s_state	<= READING_FSB;
+						jstk_y_reg(9 downto 8)	<= s_axis_tdata(1 downto 0);	-- Saving valid 2 MSBs in registers (y coord.)
+						s_state	<= READING_FSB;					-- Switching state only when data is valid
 					end if;
 
 				when READING_FSB =>
-					if s_axis_tvalid = '1' then
-						s_state	<= READING_X_L;
-						jstk_x		<= jstk_x_reg;     
-						jstk_y		<= jstk_y_reg;     
-						btn_jstk   	<= s_axis_tdata(0);   
-						btn_trigger	<= s_axis_tdata(1);
+					if s_axis_tvalid = '1' then					-- Checking buttons data validity
+						jstk_x		<= jstk_x_reg;				-- Writing the saved values of x and
+						jstk_y		<= jstk_y_reg;				-- y coord. from registers to output
+						btn_jstk   	<= s_axis_tdata(0);			-- Writing directly (and registering at the
+						btn_trigger	<= s_axis_tdata(1);			-- output) the buttons data
+
+						s_state	<= READING_X_L;					-- Since valid data was sent, getting ready
+													-- to read next packet
 					end if;
 
 				when Others => 
-					s_state <= READING_X_L; -- output sincroni ad aclk
+					s_state <= READING_X_L;		-- Only for redundancy (VHDL good habit)
 			end case; 
 		end if; 
 	end process s_FSM;
 
+	/*
+	# JSTK2 Protocol: AXI4-Stream Master, leds data from datapath to joystick
+	The sensitivity list presents only aclk and aresetn since the module is
+	synchronous with an asynchronous reset.
+	*/
 	m_FSM : process (aclk, aresetn) 
 	begin 
+		/* Asynchronous reset */
 		if aresetn = '0' then 
-			m_state 	<= RESET; 
-			m_axis_tvalid	<= '0';
-			m_axis_tdata	<= (Others => '0');
+			m_state 	<= RESET; 		-- State reset
+
+			m_axis_tvalid	<= '0';			-- tvalid='0' when aresetn='0' or when in m_state RESET or WAITING
+			m_axis_tdata	<= (Others => '0');	-- Null data output
+
 		elsif rising_edge(aclk) then 
 			case m_state is 
 				when RESET => 
-					delay_counter	<= 0;
-					m_axis_tvalid	<= '1';
-					m_axis_tdata	<= CMDSETLEDRGB;
-					m_state <= WRITE_CMD; 
+					delay_counter	<= 0;			-- Initializing the delay_counter register
 
-				when WRITE_CMD => 
-					m_axis_tvalid	<= '1';
-					if m_axis_tready = '1' then
-						m_axis_tdata	<= led_r;
-						m_state	<= WRITE_R;
+					m_axis_tvalid	<= '1';			-- tvalid='1' is set when switching state to WRITTEN_CMD
+					m_axis_tdata	<= CMDSETLEDRGB;        -- Writing the CMDSETLEDRGB in output
+					m_state <= WRITTEN_CMD;
+
+				when WRITTEN_CMD => 
+					m_axis_tvalid	<= '1';			-- Redundant, should be already '1'
+					if m_axis_tready = '1' then             -- When slave is ready to accept data:
+						m_axis_tdata	<= led_r;	-- Writing led_r to output
+						m_state	<= WRITTEN_R;		-- Switching state only when a transaction happens
 					end if;
 
-				when WRITE_R => 
-					m_axis_tvalid	<= '1';
-					if m_axis_tready = '1' then
-						m_axis_tdata	<= led_g;
-						m_state	<= WRITE_G;
+				when WRITTEN_R => 
+					m_axis_tvalid	<= '1';			-- Redundant, should be already '1'
+					if m_axis_tready = '1' then             -- When slave is ready to accept data:
+						m_axis_tdata	<= led_g;       -- Writing led_g to output
+						m_state	<= WRITTEN_G;           -- Switching state only when a transaction happens
 					end if;
 
-				when WRITE_G => 
-					m_axis_tvalid	<= '1';
-					if m_axis_tready = '1' then
-						m_axis_tdata	<= led_b;
-						m_state	<= WRITE_B;
+				when WRITTEN_G => 
+					m_axis_tvalid	<= '1';			-- Redundant, should be already '1'
+					if m_axis_tready = '1' then             -- When slave is ready to accept data:
+						m_axis_tdata	<= led_b;       -- Writing led_b to output
+						m_state	<= WRITTEN_B;           -- Switching state only when a transaction happens
 					end if;
 
-				when WRITE_B => 
-					m_axis_tvalid	<= '1';
-					if m_axis_tready = '1' then
-						m_axis_tdata	<= (Others => '0');
-						m_state	<= WRITE_DUMMY;
+				when WRITTEN_B => 
+					m_axis_tvalid	<= '1';				-- Redundant, should be already '1'
+					if m_axis_tready = '1' then                     -- When slave is ready to accept data:
+						m_axis_tdata	<= (Others => '0');     -- Writing dummy data to output
+						m_state	<= WRITTEN_DUMMY;               -- Switching state only when a transaction happens
 					end if;
 
-				when WRITE_DUMMY => 
-					m_axis_tvalid	<= '1';
-					if m_axis_tready = '1' then
-						m_axis_tvalid	<= '0';
-						m_state	<= WAITING;
+				when WRITTEN_DUMMY => 				
+					m_axis_tvalid	<= '1';			-- Redundant, should be already '1'
+					if m_axis_tready = '1' then             -- When slave is ready to accept data:
+						m_axis_tvalid	<= '0';         -- Switching to WAITING m_state, since the last transaction
+						m_state	<= WAITING;             -- has just happenend, so data during delay will not be valid
 					end if;
 
 				when WAITING => 
-					m_axis_tvalid	<= '0';
-					delay_counter	<= delay_counter + 1;
-					if delay_counter = DELAY_CYCLES - 1 then
-						delay_counter 	<= 0;
-						m_state		<= WRITE_CMD;
-						m_axis_tvalid	<= '1';
-						m_axis_tdata	<= CMDSETLEDRGB;
+					m_axis_tvalid	<= '0';				-- Redundant, should be already '0'
+                                                                                                                                                   
+					delay_counter	<= delay_counter + 1;           -- Using delay_counter to wait
+					if delay_counter = DELAY_CYCLES - 1 then        -- When delay has been accounted
+						delay_counter 	<= 0;                   -- Resetting delay_counter
+                                                                                                                                                   
+						m_axis_tvalid	<= '1';                 -- tvalid='1' is set when switching state to WRITTEN_HEADER
+						m_axis_tdata	<= CMDSETLEDRGB;        -- Writing the HEADER_CODE in output
+						m_state		<= WRITTEN_CMD;         -- Switching state only when delay has passed	           
 					end if;
 
 				when Others => 
-					m_state <= RESET; -- output sincroni ad aclk
+					m_state <= RESET;	-- Only for redundancy (VHDL good habit)
 			end case; 
 		end if; 
 	end process m_FSM;
