eaccadb5ae7d97deb3714177ae9c3cd3e38b4be8
SamueleZenoni <sam2001zen@gmail.com>
2024-05-15 15:02:35 +0200

    mute_cntroller finito
    finito

--- LAB3/modules/mute_controller.vhd
+++ LAB3/modules/mute_controller.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity mute_controller is
   Generic (
-            TDATA_WIDTH   : positive := 24
+            TDATA_WIDTH   : positive  := 24
           );
   Port    (
             aclk          : in  std_logic;
@@ -26,10 +26,45 @@ end mute_controller;
 
 architecture Behavioral of mute_controller is
 
--- signal;
+  -- Required registsers to commuicate via AXI4-S.
+  -- Furthermore, m_axis_tlast and m_axis_tvalid are basically registered
+  signal data_reg : signed(s_axis_tdata'RANGE)  := (Others => '0'); -- Register
+  signal data_out : signed(s_axis_tdata'RANGE);                     -- No register, only wire
+  signal mute_reg : std_logic                     := '0'; -- For more complex modules, also additional filter data (e.g. volume, lfo_period, ...) should be registered
+  constant MUTED  : signed(s_axis_tdata'RANGE)  := (Others => '0');
+
+
 
 begin
 
--- <=;
+  process(aclk, aresetn)
+  begin
+    if aresetn = '0' then
+      data_reg      <= (Others => '0');
+      mute_reg      <= '0';
+      m_axis_tvalid <= '0';
+      m_axis_tlast  <= '0';
+
+    elsif rising_edge(aclk) then
+      if (s_axis_tvalid and m_axis_tready) = '1' then
+        data_reg      <= signed(s_axis_tdata);
+        m_axis_tlast  <= s_axis_tlast;
+        mute_reg      <= mute;
+      end if;
+      if m_axis_tready = '1' then
+        m_axis_tvalid <= s_axis_tvalid;
+      end if;
+    end if;
+  end process;
+
+  with aresetn select s_axis_tready <=  -- Asynchronous propagation of the m_axis_tready backwards into the chain
+    m_axis_tready when '1',
+    '0'           when Others;
+
+  with mute_reg select data_out <=
+    MUTED     when '1',
+    data_reg  when Others;
+
+  m_axis_tdata  <= std_logic_vector(data_out);  -- Cast only
 
 end Behavioral;
