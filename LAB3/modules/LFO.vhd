e0eb06e8d8078943380e8d57c3e63e6185d2d8ea
luckyjack482@gmail.com <luckyjack482@gmail.com>
2024-05-20 22:05:22 +0200

    SUCCESSFULL 180MHz LFO timing

--- LAB3/modules/LFO.vhd
+++ LAB3/modules/LFO.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use IEEE.MATH_REAL."log2";
+use IEEE.MATH_REAL."ceil";
 
 entity LFO is
   Generic (
@@ -38,13 +40,14 @@ architecture Behavioral of LFO is
   constant LFO_COUNTER_BASE_PERIOD_US : integer := 1000;  -- Base period of the LFO counter in us (when the joystick is at the center)
   constant ADJUSTMENT_FACTOR          : integer := 90;    -- Multiplicative factor to scale the LFO period properly with the joystick y position: cos'è?
 
-  constant LFO_COUNTER_BASE_PERIOD    : integer := LFO_COUNTER_BASE_PERIOD_US*1000 / CLK_PERIOD_NS;
+  constant LFO_COUNTER_BASE_PERIOD    : integer := (LFO_COUNTER_BASE_PERIOD_US*1000) / CLK_PERIOD_NS;
 
   constant MIDDLE_JSTK : unsigned(JOYSTICK_LENGTH - 1 downto 0) := (JOYSTICK_LENGTH-1 => '1', Others => '0'); 
 
   -- constant MAX_TRIANGLE : unsigned(JOYSTICK_LENGTH - 1 downto 0)    := (Others => '1');
 
-  signal lfo_period_reg   : integer range 0 to (LFO_COUNTER_BASE_PERIOD + ADJUSTMENT_FACTOR* to_integer(MIDDLE_JSTK)) := 0;
+  -- signal lfo_period_reg   : integer range 0 to (LFO_COUNTER_BASE_PERIOD + ADJUSTMENT_FACTOR* to_integer(MIDDLE_JSTK)) := 0;
+  signal lfo_period_reg   : unsigned (integer(ceil(log2(real(LFO_COUNTER_BASE_PERIOD))))-1 downto 0) := (Others => '0');
   --lfo_period := LFO_COUNTER_BASE_PERIOD - ADJUSTMENT_FACTOR*joystick_y
 
   signal jstk_y_reg       : unsigned(jstk_y'RANGE) := ( Others => '0' );
@@ -52,17 +55,20 @@ architecture Behavioral of LFO is
   signal direction        : std_logic := '1'; --if '1' the slope is positive, if '0' the slope is negative.
 
   --counter and its limit value used to create the steps of the triangle wave
-  signal time_counter : integer  range 0 to LFO_COUNTER_BASE_PERIOD - ADJUSTMENT_FACTOR*(- to_integer(MIDDLE_JSTK)) := 0;
+  -- signal time_counter     : integer  range 0 to LFO_COUNTER_BASE_PERIOD - ADJUSTMENT_FACTOR*(- to_integer(MIDDLE_JSTK)) := 0;
+  signal time_counter     : unsigned (integer(ceil(log2(real(LFO_COUNTER_BASE_PERIOD))))-1 downto 0) := (Others => '0');
 
   -- Required registsers to commuicate via AXI4-S.
   -- Furthermore, m_axis_tlast and m_axis_tvalid are basically registered
   signal data_reg1          : signed(s_axis_tdata'RANGE) := (Others => '0'); -- Register
   signal data_reg2          : signed(s_axis_tdata'RANGE) := (Others => '0'); -- Register
-  signal product            : signed((triangle'LENGTH+data_reg1'LENGTH-1) downto 0)   := (Others => '0');                     -- No register, only wire
+  signal product            : signed((triangle'LENGTH+data_reg1'LENGTH-1) downto 0)   := (Others => '0');                     -- No register, only wire --PL REG
+  signal product_jstk       : unsigned(integer(ceil(log2(real( ADJUSTMENT_FACTOR * (2**JOYSTICK_LENGTH - 1) )))) - 1 downto 0)   := (Others => '0');                     -- No register, only wire --PL REG
   signal data_out           : signed(s_axis_tdata'RANGE);  -- No register, only wire
-  signal lfo_enable_reg     : std_logic                     := '0';
-  signal m_axis_tlast_reg   : std_logic                     := '0';
-  signal m_axis_tvalid_reg  : std_logic                     := '0';
+  signal lfo_enable_reg1    : std_logic                     := '0'; -- PL REG
+  signal lfo_enable_reg2    : std_logic                     := '0'; -- PL REG
+  signal m_axis_tlast_reg   : std_logic                     := '0'; -- PL REG
+  signal m_axis_tvalid_reg  : std_logic                     := '0'; -- PL REG
 
 begin
 
@@ -73,16 +79,16 @@ begin
     if aresetn = '0' then
       triangle      <= (Others => '0');
       direction     <= '1';
-      time_counter  <= 0;
+      time_counter  <= (0 => '1', Others => '0');
       jstk_y_reg    <= MIDDLE_JSTK;
-      lfo_period_reg  <= to_integer(LFO_COUNTER_BASE_PERIOD - ADJUSTMENT_FACTOR*jstk_y_reg); 
+      lfo_period_reg<= resize(LFO_COUNTER_BASE_PERIOD - ADJUSTMENT_FACTOR*MIDDLE_JSTK, lfo_period_reg'LENGTH); 
+      product_jstk  <= (Others => '0');
 
-    elsif rising_edge(aclk) and lfo_enable_reg = '1' then
-      time_counter <= time_counter + 1;
-
-      if time_counter = lfo_period_reg - 1 then
-        lfo_period_reg  <= to_integer(LFO_COUNTER_BASE_PERIOD - ADJUSTMENT_FACTOR*jstk_y_reg);
-        time_counter <= 0;
+    elsif rising_edge(aclk) and lfo_enable_reg2 = '1' then
+      if time_counter = lfo_period_reg then
+        lfo_period_reg  <= resize(LFO_COUNTER_BASE_PERIOD - product_jstk, lfo_period_reg'LENGTH);
+        time_counter <= (0 => '1', Others => '0');
+        jstk_y_reg <= unsigned(jstk_y);
 
         if direction = '1' then
           triangle <= triangle + 1;
@@ -96,8 +102,13 @@ begin
           direction <= '0';
         end if;
 
-        jstk_y_reg <= unsigned(jstk_y);
 
+      else
+        time_counter <= time_counter + 1;
+
+        -- if time_counter = lfo_period_reg - 2 then
+        product_jstk  <= resize(ADJUSTMENT_FACTOR*jstk_y_reg, product_jstk'LENGTH);
+      -- end if;
       end if;
     end if;
   end process triangle_wave;
@@ -109,7 +120,8 @@ begin
     if aresetn = '0' then
       data_reg1         <= (Others => '0');
       data_reg2         <= (Others => '0');
-      lfo_enable_reg    <= '0';
+      lfo_enable_reg1   <= '0';
+      lfo_enable_reg2   <= '0';
       m_axis_tvalid     <= '0';
       m_axis_tvalid_reg <= '0';
       m_axis_tlast      <= '0';
@@ -122,7 +134,8 @@ begin
         data_reg2         <= data_reg1;
         m_axis_tlast_reg  <= s_axis_tlast;
         m_axis_tlast      <= m_axis_tlast_reg;
-        lfo_enable_reg    <= lfo_enable;
+        lfo_enable_reg1   <= lfo_enable;
+        lfo_enable_reg2   <= lfo_enable_reg1;
       end if;
       if m_axis_tready = '1' then
         m_axis_tvalid_reg <= s_axis_tvalid;
@@ -137,7 +150,7 @@ begin
   '0'           when Others;
 
   
-  with lfo_enable_reg select data_out <=
+  with lfo_enable_reg2 select data_out <=
   resize(shift_right(product, TRIANGULAR_COUNTER_LENGTH), data_out'LENGTH)  when '1',     -- HERE the example filter is (x + 100), more complicated elaboration of the filter must be made here in datapath
   data_reg2                                                                 when Others;
 
